a567a4b26290cf398f34ba4841a628bb81e70193
William May <wjrm500@gmail.com>
2022-07-18 07:22:06 +0800

    Refactoring function for readability and using Centipawn to filter out bad black moves

--- main.py
+++ main.py
@@ -24,31 +24,32 @@ class Flashcard:
 
 flashcards = []
 def generate_flashcards(start_position, turn_depth, response_depth):
-    white_to_move = len(start_position) % 2 == 0
-    fish.set_position(start_position)
     if turn_depth == 0:
         return
-    else:
-        temp_response_depth = 1 if white_to_move else response_depth
-        for response in fish.get_top_moves(temp_response_depth):
-            if white_to_move:
-                flashcard = Flashcard(start_position, response['Move'])
-                flashcard.set_
-                flashcards.append(flashcard)
-                turn_depth -= 1
-            new_position = start_position + [response['Move']]
+    white_to_move = len(start_position) % 2 == 0
+    fish.set_position(start_position)
+    if white_to_move:
+        top_moves = fish.get_top_moves(1)
+        top_move = top_moves[0]
+        flashcard = Flashcard(start_position, top_move['Move'])
+        flashcards.append(flashcard)
+        turn_depth -= 1
+        new_position = start_position + [top_move['Move']]
+        generate_flashcards(new_position, turn_depth, response_depth)
+    else: # Black to move
+        top_moves = fish.get_top_moves(response_depth)
+        min_centipawn = min([x['Centipawn'] for x in top_moves]) # i.e. Centipawn value of black's best move
+        good_top_moves = [x for x in top_moves if x['Centipawn'] < min_centipawn + 100] # Filter out any black moves that are significantly worse than the best move
+        for top_move in good_top_moves:
+            new_position = start_position + [top_move['Move']]
             generate_flashcards(new_position, turn_depth, response_depth)
 
-# def get_centipawn(position):
-#     fish.set_position(position)
-#     return fish.get_evaluation()['value']
-
 position = ITALIAN_GAME
-td = 7
-rd = 3
+td = 3
+rd = 2
 num_flashcards = sum([rd ** n for n in range(td)])
-print(f'Generating {num_flashcards} flashcards...')
-generate_flashcards(position, td, rd)
+print(f'Generating up to {num_flashcards} flashcards...')
+generate_flashcards([], td, rd)
 sorted_flashcards = sorted(flashcards, key = lambda x: len(x.position))
 for i, flashcard in enumerate(sorted_flashcards, 1):
     print(str(i) + ': ' + str(flashcard.position) + ' | ' + flashcard.your_best_move)
