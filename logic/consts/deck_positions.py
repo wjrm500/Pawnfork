db6374ecea569908db56a62eb750c5c79ab3ebf3
William May <wjrm500@gmail.com>
2022-08-07 14:16:24 +0800

    Adding Ruy Lopez Opening

--- logic/consts/deck_positions.py
+++ logic/consts/deck_positions.py
@@ -3,4 +3,9 @@ ITALIAN_GAME = {
     'moves': ['e2e4','e7e5','g1f3','b8c6','f1c4']
 }
 
+RUY_LOPEZ_OPENING = {
+    'name': 'Ruy Lopez Opening',
+    'moves': ['e2e4', 'e7e5', 'g1f3', 'b8c6', 'f1b5']
+}
+
 deck_positions = {k: v for k, v in locals().items() if not k.startswith('__')}
