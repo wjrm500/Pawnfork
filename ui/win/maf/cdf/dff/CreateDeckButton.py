e99ab730deda2b29dd6f33a93102e83d3388a170
William May <wjrm500@gmail.com>
2022-08-07 13:05:39 +0800

    Improving data entry validation on create deck form

--- ui/win/maf/cdf/dff/CreateDeckButton.py
+++ ui/win/maf/cdf/dff/CreateDeckButton.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import List, Literal
 
 from ui.abstract.AbsButton import AbsButton
 from ui.consts.ColorConsts import ColorConsts
@@ -23,14 +24,26 @@ class CreateDeckButton(tk.Button, AbsButton):
         turn_depth = self.master.turn_depth_field_frame.field.get()
         response_depth = self.master.response_depth_field_frame.field.get()
         error_messages = []
-        if opening == '':
-            error_messages.append('Opening is required')
-        if turn_depth == '':
-            error_messages.append('Turn depth is required')
-        if response_depth == '':
-            error_messages.append('Response depth is required')
+        error_messages = self.validate_opening(error_messages, opening)
+        error_messages = self.validate_depth(error_messages, turn_depth, 'Turn')
+        error_messages = self.validate_depth(error_messages, response_depth, 'Response')
         if len(error_messages):
             error_message = '\n'.join(error_messages)
             self.master.show_error(error_message)
         else:
             pass
+    
+    def validate_opening(self, error_messages: List[str], opening: str) -> str:
+        if opening == '':
+            error_messages.append('Opening is required')
+        return error_messages
+    
+    def validate_depth(self, error_messages: List[str], depth: int, type: Literal['Turn', 'Response']) -> None:
+        if depth == '':
+            return error_messages + [f'{type} depth is required']
+        if not depth.isnumeric():
+            return error_messages + [f'{type} depth must be numeric']
+        max_depth = 10
+        if int(depth) > max_depth:
+            return error_messages + [f'{type} depth cannot exceed {max_depth}']
+        return error_messages
