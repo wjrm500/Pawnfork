9553e2dc56bfb7402280ecd155f863b2bb478f2a
William May <wjrm500@gmail.com>
2022-08-17 02:30:50 +0100

    Adding two new openings - Kings Pawn and Queens Pawn

--- logic/consts/openings.py
+++ logic/consts/openings.py
@@ -3,9 +3,19 @@ CENTER_GAME_ACCEPTED_QXD4 = {
     'moves': ['e2e4', 'e7e5', 'd2d4', 'e5d4', 'd1d4']
 }
 
+KINGS_PAWN_OPENING = {
+    'name': 'King\'s Pawn Opening',
+    'moves': ['e2e4', 'e7e5']
+}
+
 ITALIAN_GAME = {
     'name': 'Italian Game',
     'moves': ['e2e4','e7e5','g1f3','b8c6','f1c4']
+}
+
+QUEENS_PAWN_OPENING = {
+    'name': 'Queen\'s Pawn Opening',
+    'moves': ['d2d4', 'd7d5']
 }
 
 RUY_LOPEZ_OPENING = {
