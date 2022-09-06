fe30ef622862cd74dc59bccbaaeba4d4e3ba3d3a
William May <wjrm500@gmail.com>
2022-09-06 04:18:09 +0100

    Adding some openings

--- logic/consts/openings.py
+++ logic/consts/openings.py
@@ -1,6 +1,26 @@
-CENTER_GAME_ACCEPTED_QXD4 = {
-    'name': 'Center Game Accepted: 3.Qxd4',
-    'moves': ['e2e4', 'e7e5', 'd2d4', 'e5d4', 'd1d4']
+CARO_KANN_DEFENCE = {
+    'name': 'Caro-Kann Defence',
+    'moves': ['e2e4', 'c7c6']
+}
+
+CENTER_GAME = {
+    'name': 'Center Game',
+    'moves': ['e2e4', 'e7e5', 'd2d4', 'e5d4']
+}
+
+ENGLISH_OPENING = {
+    'name': 'English Opening',
+    'moves': ['c2c4']
+}
+
+FRENCH_DEFENCE = {
+    'name': 'French Defence',
+    'moves': ['e2e4', 'e7e6']
+}
+
+KINGS_GAMBIT = {
+    'name': 'King\'s Gambit',
+    'moves': ['e2e4', 'e7e5', 'f2f4']
 }
 
 KINGS_PAWN_OPENING = {
@@ -11,6 +31,16 @@ KINGS_PAWN_OPENING = {
 ITALIAN_GAME = {
     'name': 'Italian Game',
     'moves': ['e2e4','e7e5','g1f3','b8c6','f1c4']
+}
+
+PIRC_DEFENCE = {
+    'name': 'Pirc Defence',
+    'moves': ['e2e4', 'd7d6']
+}
+
+QUEENS_GAMBIT = {
+    'name': 'Queen\'s Gambit',
+    'moves': ['d2d4', 'd7d5', 'c2c4']
 }
 
 QUEENS_PAWN_OPENING = {
